51200d7eae9b1360f055a0852925a4697bc133c3
jdbhansen <blochhest@youmail.dk>
2021-06-14 19:13:41 +0200

    yesh

--- OrderlineConverter/OrderlineConverter/Implementations/Item.cs
+++ OrderlineConverter/OrderlineConverter/Implementations/Item.cs
@@ -1,9 +1,5 @@
 ﻿using OrderlineConverter.OrderlineConverter.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrderlineConverter.OrderlineConverter.Implementations
 {
--- OrderlineConverter/OrderlineConverter/Implementations/StringToItemsConverter.cs
+++ OrderlineConverter/OrderlineConverter/Implementations/StringToItemsConverter.cs
@@ -1,9 +1,6 @@
 ﻿using OrderlineConverter.OrderlineConverter.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrderlineConverter.OrderlineConverter.Implementations
 {
